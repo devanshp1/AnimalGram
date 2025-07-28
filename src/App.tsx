4e4b77373e407ace5030414b5f5da371e629d602
Devansh Pal <devanshpv1@gmail.com>
2025-07-28 23:08:39 +0530

    feat: setup shadcn and tailwind custom utilities

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+
 import './globals.css';
+import AuthLayout from './_auth/AuthLayout';
+import SignInForm from './_auth/forms/SignInForm';
+import SignUpForm from './_auth/forms/SignUpForm';
+import RootLayout from './_root/RootLayout';
+import Home from './_root/pages/Home';
 
 const App = () => {
+    return (
+        <main className="flex w-full h-screen">
+            <Routes>
+                {/* Public Routes */}
+                <Route element={<AuthLayout />}>
+                    <Route path="sign-in" element={<SignInForm />} />
+                    <Route path="sign-up" element={<SignUpForm />} />
+                </Route>
+                {/* Private Routes */}
+                <Route element={<RootLayout />}>
+                    <Route path="/" element={<Home />} />
+                </Route>
+            </Routes>
+        </main>
+    )
     return (
         <h1 className="flex bg-blue-500 text-red-400">
             App
---
+++ src/_auth/forms/SignUpForm.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+const SignUpForm = () => {
+    return (
+        <Button>Click</Button>
+    )
+}
+
+export default SignUpForm
